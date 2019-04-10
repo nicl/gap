e67a0aa320fd32ae0cb44f57954a264d316e1004
Nicolas Long <nicolaslong@gmail.com>
2019-04-10 13:38:16 +0100

    Update webpack for gap-display

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,8 @@ module.exports = () => ({
         "gap-list": "./src/gap-list/gap-list.ts",
         "gap-proxy": "./src/gap-proxy/gap-proxy.ts",
         "gap-ab": "./src/gap-ab/gap-ab.ts",
-        "gap-slot": "./src/gap-slot/gap-slot.ts"
+        "gap-slot": "./src/gap-slot/gap-slot.ts",
+        "gap-display": "./src/gap-display/gap-display.ts"
     },
     output: {
         path: resolve(__dirname, `./dist/`),
