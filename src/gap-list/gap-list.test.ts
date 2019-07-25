755047174faf09e5458487bc3c509f8c54a29d4a
Nicolas Long <nicolas.long@theguardian.com>
2019-07-25 12:11:32 +0100

    Fix gap-list test

--- src/gap-list/gap-list.test.ts
+++ src/gap-list/gap-list.test.ts
@@ -1,16 +1,16 @@
-import GapList from './gap-list';
-import { defaultHelpers } from '../gap-core/gap-core';
+import GapList from "./gap-list";
+import { defaultHelpers } from "../gap-core/gap-core";
 
-describe('gap-list', () => {
-    it('should populate the template', async () => {
+describe("gap-list", () => {
+    it("should populate the template", async () => {
         const globalAny: any = global;
         const resp = {
             ok: true,
-            json: () => ({ foo: 1 }),
+            json: () => ({ foo: 1 })
         };
         globalAny.fetch = jest.fn(async () => resp);
-        document.body.innerHTML = `<gap-list id='test' data-src='example.json'><template>[[ foo ]]</template></gap-list>`;
-        const el = document.getElementById('test');
+        document.body.innerHTML = `<gap-list id='test' data-src='example.json'><template>{{ foo }}</template></gap-list>`;
+        const el = document.getElementById("test");
 
         if (el === null) {
             fail();
@@ -18,6 +18,6 @@ describe('gap-list', () => {
         }
 
         await GapList.do(el as Element, defaultHelpers);
-        expect(el.innerHTML).toBe('1');
+        expect(el.innerHTML).toBe("1");
     });
 });
